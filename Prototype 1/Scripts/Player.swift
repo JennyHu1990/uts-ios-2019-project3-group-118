ee8a98b3894321b11c5b4ccda150d54a8a67cf2b
siouwun <12793461@student.uts.edu.au>
2019-05-20 17:00:04 +1000

    complete player class

--- Prototype 1/Scripts/Player.swift
+++ Prototype 1/Scripts/Player.swift
@@ -12,9 +12,34 @@ import GameplayKit
 
 enum PlayerType: Int {
     case player1,
-    player2
+    player2,
+    player3
 }
 
 class Player: SKSpriteNode {
-
+    let player :PlayerType
+    let frontTexture :SKTexture
+    let backTexture :SKTexture
+    var hp: Int
+   
+    
+    init(health: Int, playerType: PlayerType) {
+        self.player = playerType
+        backTexture = SKTexture(imageNamed: "CardBackgroundShadow")
+        self.hp = health
+        switch playerType {
+        case .player1:
+            frontTexture = SKTexture(imageNamed: "Enemy2")
+        case .player2:
+            frontTexture = SKTexture(imageNamed: "ManaCircle")
+        case .player3:
+            frontTexture = SKTexture(imageNamed: "ManaCircle")
+        }
+        
+        super.init(texture: frontTexture, color: .clear, size: frontTexture.size())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
